dfb6d608f4efae47bb30e7a94f63f0e9e4b5fcff
Liudmyla Reva <lyudareva1@gmail.com>
2026-09-08 13:30:05 +0200

    feat: creacion inicial de la clase PracticaGit

---
+++ PracticaGit.java
@@ -0,0 +1,13 @@
+public class PracticaGit {
+
+    public static void main(String[] args) {
+        int cantidadArtículos = 120;
+        String nombreProducto = "Bebidas";
+        double precioUnidad = 12.68;
+
+        System.out.println("Producto: " + nombreProducto);
+        System.out.println("Cantidad: " + cantidadArtículos);
+        System.out.println("Precio: " + precioUnidad + "€");
+
+    }
+}
